312d2558343c670e6ff1fa40c91057ba595323f8
eivy <eivyromero.30@gmail.com>
2026-01-21 10:00:58 -0500

    Test view without template to debug 500

--- tasks/views.py
+++ tasks/views.py
@@ -1,13 +1,4 @@
-from django.shortcuts import render
-from .models import Skill, Project, Education
+from django.http import HttpResponse
 
 def home(request):
-    skills = Skill.objects.all()
-    projects = Project.objects.all()
-    education = Education.objects.all()
-
-    return render(request, "home.html", {
-        "skills": skills,
-        "projects": projects,
-        "education": education,
-    })
+    return HttpResponse("La vista funciona correctamente ✅")
