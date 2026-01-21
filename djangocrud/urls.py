8c143f8ac3d421575e5fa1055e73b136dd5148de
eivy <eivyromero.30@gmail.com>
2026-01-21 09:43:30 -0500

    Fix admin URL not found

--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -1,11 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
-from tasks.views import create_admin
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
+from tasks.views import home
+
 urlpatterns = [
-    path('', include('tasks.urls')),
-    path("create-admin/", create_admin),
+    path('admin/', admin.site.urls),  # 👈 ESTA LÍNEA ES CLAVE
+    path('', home, name='home'),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
