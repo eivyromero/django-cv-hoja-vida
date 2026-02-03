94ec47dc5091bdf843f578f25735ce16e79a72db
eivyromero <eivyromero.26@gmail.com>
2026-02-03 10:54:36 -0500

    Fix WhiteNoise configuration for Render deploy

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -50,11 +50,13 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # ⭐ AGREGAR ESTA LÍNEA
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',  # ⭐ AGREGAR ESTA LÍNEA
 ]
 
 # --------------------------------------------------
